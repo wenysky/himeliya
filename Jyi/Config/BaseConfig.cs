8fce56acaf49895d94145a0a50fe7b16555dcb48
wenysky <wenysky@75c35ffa-ff29-11dd-984d-ab748d24aa7e>
2009-02-23 09:59:34 +0000

    挂接新的代理列表文件读取

--- Jyi/Config/BaseConfig.cs
+++ Jyi/Config/BaseConfig.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Jyi.Entity;
 using System.Xml;
+using System.IO;
 
 namespace Jyi.Config
 {
@@ -15,18 +16,28 @@ namespace Jyi.Config
             xmlDoc.Load(MainForm.configFilePath);
             XmlNode xnBaseConfig = xmlDoc.SelectSingleNode("/JyiConfig");
             #region Proxy
-            XmlNodeList xnlProxys = xnBaseConfig.SelectSingleNode("Proxy").ChildNodes;
-
+            List<Natsuhime.Proxy.ProxyInfo> list = Natsuhime.Proxy.ProxyUtility.GetProxyList_FromConfig(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ProxyList.dat"));
             List<ProxyInfo> objProxyList = new List<ProxyInfo>();
-            foreach (XmlNode xnf in xnlProxys)
+            foreach (Natsuhime.Proxy.ProxyInfo info in list)
             {
-                ProxyInfo objProxy = new ProxyInfo();
-                XmlElement xe = (XmlElement)xnf;
-                objProxy.Name = xnf.InnerText;
-                objProxy.Address = xnf.Attributes["Address"].Value;
-                objProxy.Port = int.Parse(xnf.Attributes["Port"].Value);
-                objProxyList.Add(objProxy);
+                ProxyInfo pi = new ProxyInfo();
+                pi.Name = info.Name;
+                pi.Address = info.Address;
+                pi.Port = info.Port;
+                objProxyList.Add(pi);
             }
+            //XmlNodeList xnlProxys = xnBaseConfig.SelectSingleNode("Proxy").ChildNodes;
+
+            //List<ProxyInfo> objProxyList = new List<ProxyInfo>();
+            //foreach (XmlNode xnf in xnlProxys)
+            //{
+            //    ProxyInfo objProxy = new ProxyInfo();
+            //    XmlElement xe = (XmlElement)xnf;
+            //    objProxy.Name = xnf.InnerText;
+            //    objProxy.Address = xnf.Attributes["Address"].Value;
+            //    objProxy.Port = int.Parse(xnf.Attributes["Port"].Value);
+            //    objProxyList.Add(objProxy);
+            //}
             objBaseConfig.ProxyList = objProxyList;
             #endregion
 
