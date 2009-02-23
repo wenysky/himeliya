6c2bd03becd6c449af028e49426a2eedf1a5d577
wenysky <wenysky@75c35ffa-ff29-11dd-984d-ab748d24aa7e>
2009-02-23 09:59:34 +0000

    挂接新的代理列表文件读取

--- trunk/Jyi/Entity/ProxyInfo.cs
+++ trunk/Jyi/Entity/ProxyInfo.cs
@@ -2,28 +2,10 @@
 
 namespace Jyi.Entity
 {
-    class ProxyInfo
+    class ProxyInfo : Natsuhime.Proxy.ProxyInfo
     {
-        private string m_Name;
-        private string m_Address;
-        private int m_Port;
         private bool m_Checking = false;
 
-        public string Name
-        {
-            get { return m_Name; }
-            set { m_Name = value; }
-        }
-        public string Address
-        {
-            get { return m_Address; }
-            set { m_Address = value; }
-        }
-        public int Port
-        {
-            get { return m_Port; }
-            set { m_Port = value; }
-        }
         public bool HaveGet
         {
             get { return m_Checking; }
