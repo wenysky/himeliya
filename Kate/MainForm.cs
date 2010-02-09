e7aa4bd1a30283357d6ae9a2d4afeb34368f24f6
wenysky <wenysky@75c35ffa-ff29-11dd-984d-ab748d24aa7e>
2010-02-09 05:30:39 +0000

    将部分代码迁移到dis类库,httper使用异步处理结果

--- Kate/MainForm.cs
+++ Kate/MainForm.cs
@@ -9,52 +9,104 @@ using Himeliya.Controls;
 using Natsuhime;
 using System.Net;
 using System.Text.RegularExpressions;
+using Natsuhime.Text;
+using Natsuhime.Web;
 
 namespace Himeliya.Kate
 {
     public partial class MainForm : Form
     {
-        CookieContainer cookie = null;
-        Dictionary<string, string> urlList = null;
         List<string> downloadUrlList;
+
+        NewHttper nhttper = null;
 
         public MainForm()
         {
             InitializeComponent();
-            cookie = new CookieContainer();
+
+            this.nhttper = new NewHttper();
+            this.nhttper.Cookie = new CookieContainer();
+            this.nhttper.RequestStringCompleted += new NewHttper.RequestStringCompleteEventHandler(nhttper_RequestStringCompleted);
         }
 
-        private void btnGetLinks_Click(object sender, EventArgs e)
+        #region 处理过程
+        void nhttper_RequestStringCompleted(object sender, RequestStringCompletedEventArgs e)
         {
-            Httper httper = new Httper();
-            httper.Url = tbxUrl.Text.Trim();
-            httper.Charset = httper.GetPageLanguageCode();
-            httper.Cookie = cookie;
-            string returnData = httper.HttpGet();
-
-            MatchCollection mc = RegexUtility.GetMatchFull(returnData, "<span id=\"thread_[0-9]+\"><a href=\"(.*?)\".*>(.*)</a>");
-            if (mc != null)
+            if (e.Error == null)
             {
-                urlList = new Dictionary<string, string>();
-                foreach (Match m in mc)
-                {
-                    urlList.Add(m.Groups[2].Value, m.Groups[1].Value);
-                }
+                string baseUrl = tbxUrl.Text.Trim().Substring(0, tbxUrl.Text.Trim().LastIndexOf('/') + 1);
 
-                foreach (string key in urlList.Keys)
+                if (e.UserState.ToString() == "GetTitleUrls")
                 {
-                    tbxMessage.Text += key + "\r\n" + tbxUrl.Text.Trim().Substring(0, tbxUrl.Text.Trim().LastIndexOf('/') + 1) + urlList[key] + "\r\n\r\n";
+                    GetTitleUrlsComplete(e.ResponseString, baseUrl);
+                }
+                else if (e.UserState.ToString() == "GetImageUrls")
+                {
+                    GetImageUrlsComplete(e.ResponseString, baseUrl);
                 }
             }
             else
             {
-                tbxMessage.Text = "no regexed";
-                return;
+                MessageBox.Show(e.Error.Message);
             }
+        }
+
+        void GetImageUrlsComplete(string sourceHtml, string baseUrl)
+        {
+            string regexString = RegexStringLib.GetFileUrlRegexString("jpg|jpeg|png|ico|bmp|gif");
+            List<string> imageUrls = TextAnalyze.GetUrlList(sourceHtml, regexString, baseUrl);
+
+            if (imageUrls.Count > 0)
+            {
+                this.downloadUrlList = new List<string>();
+                foreach (string url in imageUrls)
+                {
+                    string fullUrl = Utils.CompleteRelativeUrl(baseUrl, url);
+                    this.downloadUrlList.Add(fullUrl);
+
+                    tbxMessage.Text += string.Format("{1}{0}", Environment.NewLine, fullUrl);
+                }
+                MessageBox.Show(string.Format("{0} images got!", imageUrls.Count));
+            }
+            else
+            {
+                tbxMessage.Text = "no regexed";
+            }
+        }
+
+        void GetTitleUrlsComplete(string sourceHtml, string baseUrl)
+        {
+            Dictionary<string, string> urlList = Natsuhime.Web.Plugin.Discuz.TextAnalyze.GetThreadsInBoard(sourceHtml);
+            if (urlList.Count > 0)
+            {
+                foreach (string key in urlList.Keys)
+                {
+                    string fullUrl = Utils.CompleteRelativeUrl(baseUrl, urlList[key]);
+
+                    tbxMessage.Text += string.Format("{1}{0}{2}{0}{0}", Environment.NewLine, key, fullUrl);
+                }
+                MessageBox.Show(string.Format("{0} threads got!", urlList.Count));
+            }
+            else
+            {
+                tbxMessage.Text = "no regexed";
+            }
+        }
+        #endregion
+
+
+
+
+        private void btnGetThreadLinks_Click(object sender, EventArgs e)
+        {
+            this.nhttper.Url = tbxUrl.Text.Trim();
+            this.nhttper.Charset = nhttper.GetPageLanguageCode();
+            this.nhttper.RequestStringAsync(EnumRequestMethod.GET, "GetTitleUrls");
         }
 
         private void btnGetPosts_Click(object sender, EventArgs e)
         {
+            #region 保留的注释
             /*
             Httper httper = new Httper();
             httper.Url = tbxUrl.Text.Trim();
@@ -83,16 +135,10 @@ namespace Himeliya.Kate
                             return;
                         }
                          */
-            string ImageType = "jpg|jpeg|png|ico|bmp|gif";
-            List<string> result = TextAnalyze.GetUrlListByUrl(tbxUrl.Text.Trim(), @"((http(s)?://)?)+(((/?)+[\w-.]+(/))*)+[\w-./]+\.+(" + ImageType + ")");
-
-            downloadUrlList = result;
-
-            foreach (string link in result)
-            {
-                tbxMessage.Text += link + "\r\n";
-            }
-            tbxMessage.Text += "OK";
+            #endregion
+            this.nhttper.Url = tbxUrl.Text.Trim();
+            this.nhttper.Charset = nhttper.GetPageLanguageCode();
+            this.nhttper.RequestStringAsync(EnumRequestMethod.GET, "GetImageUrls");
         }
 
         private void btnStartDownload_Click(object sender, EventArgs e)
@@ -104,7 +150,7 @@ namespace Himeliya.Kate
             {
                 dr.Download();
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 tbxMessage.Text += ex.Message + "\r\n";
             }
