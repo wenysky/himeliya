9573c2cf62eb2236d49224aaf96c91ea09634337
wenysky <wenysky@75c35ffa-ff29-11dd-984d-ab748d24aa7e>
2010-02-10 09:58:01 +0000

    完成工程设置

--- Kate/ConfigForm.Designer.cs
+++ Kate/ConfigForm.Designer.cs
@@ -30,24 +30,27 @@
         {
             this.lblProxyAdd = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.tbxProxyAddress = new System.Windows.Forms.TextBox();
-            this.tbxProxyPort = new System.Windows.Forms.TextBox();
-            this.lblProxyPort = new System.Windows.Forms.Label();
             this.ckbxIsUserProxy = new System.Windows.Forms.CheckBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.cbbxProjects = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.tbxUrl = new System.Windows.Forms.TextBox();
-            this.tbxTotalPageCount = new System.Windows.Forms.TextBox();
-            this.tbxCurrentPageId = new System.Windows.Forms.TextBox();
-            this.tbxCurrentId = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.btnNewProject = new Himeliya.Controls.Button();
-            this.btnSaveSelectProject = new Himeliya.Controls.Button();
             this.btnSaveNetwork = new Himeliya.Controls.Button();
+            this.lblProxyPort = new System.Windows.Forms.Label();
+            this.tbxProxyPort = new System.Windows.Forms.TextBox();
+            this.tbxProxyAddress = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.ckbxEditProject = new System.Windows.Forms.CheckBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbxProjectName = new System.Windows.Forms.TextBox();
+            this.btnNewProject = new Himeliya.Controls.Button();
+            this.btnSaveProjectInfo = new Himeliya.Controls.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxCurrentPostId = new System.Windows.Forms.TextBox();
+            this.tbxCurrentPageId = new System.Windows.Forms.TextBox();
+            this.tbxTotalPageCount = new System.Windows.Forms.TextBox();
+            this.tbxUrl = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbbxProjects = new System.Windows.Forms.ComboBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
@@ -71,24 +74,31 @@
             this.groupBox1.Controls.Add(this.lblProxyAdd);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(288, 208);
+            this.groupBox1.Size = new System.Drawing.Size(536, 57);
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "网络";
             // 
-            // tbxProxyAddress
+            // ckbxIsUserProxy
             // 
-            this.tbxProxyAddress.Location = new System.Drawing.Point(48, 20);
-            this.tbxProxyAddress.Name = "tbxProxyAddress";
-            this.tbxProxyAddress.Size = new System.Drawing.Size(136, 21);
-            this.tbxProxyAddress.TabIndex = 2;
+            this.ckbxIsUserProxy.AutoSize = true;
+            this.ckbxIsUserProxy.Location = new System.Drawing.Point(362, 22);
+            this.ckbxIsUserProxy.Name = "ckbxIsUserProxy";
+            this.ckbxIsUserProxy.Size = new System.Drawing.Size(72, 16);
+            this.ckbxIsUserProxy.TabIndex = 5;
+            this.ckbxIsUserProxy.Text = "使用代理";
+            this.ckbxIsUserProxy.UseVisualStyleBackColor = true;
             // 
-            // tbxProxyPort
+            // btnSaveNetwork
             // 
-            this.tbxProxyPort.Location = new System.Drawing.Point(241, 20);
-            this.tbxProxyPort.Name = "tbxProxyPort";
-            this.tbxProxyPort.Size = new System.Drawing.Size(41, 21);
-            this.tbxProxyPort.TabIndex = 3;
+            this.btnSaveNetwork.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSaveNetwork.Location = new System.Drawing.Point(440, 20);
+            this.btnSaveNetwork.Name = "btnSaveNetwork";
+            this.btnSaveNetwork.Size = new System.Drawing.Size(84, 23);
+            this.btnSaveNetwork.TabIndex = 0;
+            this.btnSaveNetwork.Text = "应用";
+            this.btnSaveNetwork.UseVisualStyleBackColor = true;
+            this.btnSaveNetwork.Click += new System.EventHandler(this.btnSaveNetwork_Click);
             // 
             // lblProxyPort
             // 
@@ -99,109 +109,93 @@
             this.lblProxyPort.TabIndex = 4;
             this.lblProxyPort.Text = "端口:";
             // 
-            // ckbxIsUserProxy
+            // tbxProxyPort
             // 
-            this.ckbxIsUserProxy.AutoSize = true;
-            this.ckbxIsUserProxy.Location = new System.Drawing.Point(112, 186);
-            this.ckbxIsUserProxy.Name = "ckbxIsUserProxy";
-            this.ckbxIsUserProxy.Size = new System.Drawing.Size(72, 16);
-            this.ckbxIsUserProxy.TabIndex = 5;
-            this.ckbxIsUserProxy.Text = "使用代理";
-            this.ckbxIsUserProxy.UseVisualStyleBackColor = true;
+            this.tbxProxyPort.Location = new System.Drawing.Point(241, 20);
+            this.tbxProxyPort.Name = "tbxProxyPort";
+            this.tbxProxyPort.Size = new System.Drawing.Size(41, 21);
+            this.tbxProxyPort.TabIndex = 3;
+            // 
+            // tbxProxyAddress
+            // 
+            this.tbxProxyAddress.Location = new System.Drawing.Point(48, 20);
+            this.tbxProxyAddress.Name = "tbxProxyAddress";
+            this.tbxProxyAddress.Size = new System.Drawing.Size(136, 21);
+            this.tbxProxyAddress.TabIndex = 2;
             // 
             // groupBox2
             // 
+            this.groupBox2.Controls.Add(this.ckbxEditProject);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.tbxProjectName);
             this.groupBox2.Controls.Add(this.btnNewProject);
-            this.groupBox2.Controls.Add(this.btnSaveSelectProject);
+            this.groupBox2.Controls.Add(this.btnSaveProjectInfo);
             this.groupBox2.Controls.Add(this.label5);
             this.groupBox2.Controls.Add(this.label4);
             this.groupBox2.Controls.Add(this.label3);
             this.groupBox2.Controls.Add(this.label2);
-            this.groupBox2.Controls.Add(this.tbxCurrentId);
+            this.groupBox2.Controls.Add(this.tbxCurrentPostId);
             this.groupBox2.Controls.Add(this.tbxCurrentPageId);
             this.groupBox2.Controls.Add(this.tbxTotalPageCount);
             this.groupBox2.Controls.Add(this.tbxUrl);
             this.groupBox2.Controls.Add(this.label1);
             this.groupBox2.Controls.Add(this.cbbxProjects);
-            this.groupBox2.Location = new System.Drawing.Point(306, 12);
+            this.groupBox2.Location = new System.Drawing.Point(12, 75);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(242, 208);
+            this.groupBox2.Size = new System.Drawing.Size(536, 205);
             this.groupBox2.TabIndex = 3;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "项目";
             // 
-            // cbbxProjects
+            // ckbxEditProject
             // 
-            this.cbbxProjects.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cbbxProjects.FormattingEnabled = true;
-            this.cbbxProjects.Location = new System.Drawing.Point(47, 20);
-            this.cbbxProjects.Name = "cbbxProjects";
-            this.cbbxProjects.Size = new System.Drawing.Size(189, 20);
-            this.cbbxProjects.TabIndex = 0;
+            this.ckbxEditProject.AutoSize = true;
+            this.ckbxEditProject.Location = new System.Drawing.Point(415, 180);
+            this.ckbxEditProject.Name = "ckbxEditProject";
+            this.ckbxEditProject.Size = new System.Drawing.Size(48, 16);
+            this.ckbxEditProject.TabIndex = 6;
+            this.ckbxEditProject.Text = "修改";
+            this.ckbxEditProject.UseVisualStyleBackColor = true;
+            this.ckbxEditProject.CheckedChanged += new System.EventHandler(this.ckbxEditProject_CheckedChanged);
             // 
-            // label1
+            // label6
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 24);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 12);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "项目:";
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 49);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 12);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "名字:";
             // 
-            // tbxUrl
+            // tbxProjectName
             // 
-            this.tbxUrl.Location = new System.Drawing.Point(47, 71);
-            this.tbxUrl.Name = "tbxUrl";
-            this.tbxUrl.Size = new System.Drawing.Size(189, 21);
-            this.tbxUrl.TabIndex = 6;
+            this.tbxProjectName.Location = new System.Drawing.Point(47, 46);
+            this.tbxProjectName.Name = "tbxProjectName";
+            this.tbxProjectName.ReadOnly = true;
+            this.tbxProjectName.Size = new System.Drawing.Size(477, 21);
+            this.tbxProjectName.TabIndex = 18;
             // 
-            // tbxTotalPageCount
+            // btnNewProject
             // 
-            this.tbxTotalPageCount.Location = new System.Drawing.Point(47, 98);
-            this.tbxTotalPageCount.Name = "tbxTotalPageCount";
-            this.tbxTotalPageCount.Size = new System.Drawing.Size(189, 21);
-            this.tbxTotalPageCount.TabIndex = 7;
+            this.btnNewProject.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnNewProject.Location = new System.Drawing.Point(9, 176);
+            this.btnNewProject.Name = "btnNewProject";
+            this.btnNewProject.Size = new System.Drawing.Size(55, 23);
+            this.btnNewProject.TabIndex = 17;
+            this.btnNewProject.Text = "新建";
+            this.btnNewProject.UseVisualStyleBackColor = true;
+            this.btnNewProject.Click += new System.EventHandler(this.btnNewProject_Click);
             // 
-            // tbxCurrentPageId
+            // btnSaveProjectInfo
             // 
-            this.tbxCurrentPageId.Location = new System.Drawing.Point(47, 125);
-            this.tbxCurrentPageId.Name = "tbxCurrentPageId";
-            this.tbxCurrentPageId.Size = new System.Drawing.Size(189, 21);
-            this.tbxCurrentPageId.TabIndex = 8;
-            // 
-            // tbxCurrentId
-            // 
-            this.tbxCurrentId.Location = new System.Drawing.Point(47, 152);
-            this.tbxCurrentId.Name = "tbxCurrentId";
-            this.tbxCurrentId.Size = new System.Drawing.Size(189, 21);
-            this.tbxCurrentId.TabIndex = 9;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(6, 74);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 12);
-            this.label2.TabIndex = 10;
-            this.label2.Text = "地址:";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(6, 101);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 12);
-            this.label3.TabIndex = 11;
-            this.label3.Text = "总页:";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(6, 128);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 12);
-            this.label4.TabIndex = 12;
-            this.label4.Text = "此页:";
+            this.btnSaveProjectInfo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSaveProjectInfo.Location = new System.Drawing.Point(469, 176);
+            this.btnSaveProjectInfo.Name = "btnSaveProjectInfo";
+            this.btnSaveProjectInfo.Size = new System.Drawing.Size(55, 23);
+            this.btnSaveProjectInfo.TabIndex = 16;
+            this.btnSaveProjectInfo.Text = "应用";
+            this.btnSaveProjectInfo.UseVisualStyleBackColor = true;
+            this.btnSaveProjectInfo.Click += new System.EventHandler(this.btnSaveProjectInfo_Click);
             // 
             // label5
             // 
@@ -212,42 +206,89 @@
             this.label5.TabIndex = 13;
             this.label5.Text = "此id:";
             // 
-            // btnNewProject
+            // label4
             // 
-            this.btnNewProject.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnNewProject.Location = new System.Drawing.Point(8, 181);
-            this.btnNewProject.Name = "btnNewProject";
-            this.btnNewProject.Size = new System.Drawing.Size(55, 23);
-            this.btnNewProject.TabIndex = 17;
-            this.btnNewProject.Text = "新建";
-            this.btnNewProject.UseVisualStyleBackColor = true;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 12);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "此页:";
             // 
-            // btnSaveSelectProject
+            // label3
             // 
-            this.btnSaveSelectProject.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnSaveSelectProject.Location = new System.Drawing.Point(181, 179);
-            this.btnSaveSelectProject.Name = "btnSaveSelectProject";
-            this.btnSaveSelectProject.Size = new System.Drawing.Size(55, 23);
-            this.btnSaveSelectProject.TabIndex = 16;
-            this.btnSaveSelectProject.Text = "应用";
-            this.btnSaveSelectProject.UseVisualStyleBackColor = true;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 12);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "总页:";
             // 
-            // btnSaveNetwork
+            // label2
             // 
-            this.btnSaveNetwork.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnSaveNetwork.Location = new System.Drawing.Point(198, 179);
-            this.btnSaveNetwork.Name = "btnSaveNetwork";
-            this.btnSaveNetwork.Size = new System.Drawing.Size(84, 23);
-            this.btnSaveNetwork.TabIndex = 0;
-            this.btnSaveNetwork.Text = "应用";
-            this.btnSaveNetwork.UseVisualStyleBackColor = true;
-            this.btnSaveNetwork.Click += new System.EventHandler(this.btnSaveNetwork_Click);
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "地址:";
+            // 
+            // tbxCurrentPostId
+            // 
+            this.tbxCurrentPostId.Location = new System.Drawing.Point(47, 152);
+            this.tbxCurrentPostId.Name = "tbxCurrentPostId";
+            this.tbxCurrentPostId.ReadOnly = true;
+            this.tbxCurrentPostId.Size = new System.Drawing.Size(477, 21);
+            this.tbxCurrentPostId.TabIndex = 9;
+            // 
+            // tbxCurrentPageId
+            // 
+            this.tbxCurrentPageId.Location = new System.Drawing.Point(47, 125);
+            this.tbxCurrentPageId.Name = "tbxCurrentPageId";
+            this.tbxCurrentPageId.ReadOnly = true;
+            this.tbxCurrentPageId.Size = new System.Drawing.Size(477, 21);
+            this.tbxCurrentPageId.TabIndex = 8;
+            // 
+            // tbxTotalPageCount
+            // 
+            this.tbxTotalPageCount.Location = new System.Drawing.Point(47, 98);
+            this.tbxTotalPageCount.Name = "tbxTotalPageCount";
+            this.tbxTotalPageCount.ReadOnly = true;
+            this.tbxTotalPageCount.Size = new System.Drawing.Size(477, 21);
+            this.tbxTotalPageCount.TabIndex = 7;
+            // 
+            // tbxUrl
+            // 
+            this.tbxUrl.Location = new System.Drawing.Point(47, 71);
+            this.tbxUrl.Name = "tbxUrl";
+            this.tbxUrl.ReadOnly = true;
+            this.tbxUrl.Size = new System.Drawing.Size(477, 21);
+            this.tbxUrl.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "项目:";
+            // 
+            // cbbxProjects
+            // 
+            this.cbbxProjects.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbbxProjects.FormattingEnabled = true;
+            this.cbbxProjects.Location = new System.Drawing.Point(47, 20);
+            this.cbbxProjects.Name = "cbbxProjects";
+            this.cbbxProjects.Size = new System.Drawing.Size(477, 20);
+            this.cbbxProjects.TabIndex = 0;
+            this.cbbxProjects.SelectedIndexChanged += new System.EventHandler(this.cbbxProjects_SelectedIndexChanged);
             // 
             // ConfigForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(560, 232);
+            this.ClientSize = new System.Drawing.Size(560, 292);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Name = "ConfigForm";
@@ -274,14 +315,17 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox cbbxProjects;
         private Himeliya.Controls.Button btnNewProject;
-        private Himeliya.Controls.Button btnSaveSelectProject;
+        private Himeliya.Controls.Button btnSaveProjectInfo;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox tbxCurrentId;
+        private System.Windows.Forms.TextBox tbxCurrentPostId;
         private System.Windows.Forms.TextBox tbxCurrentPageId;
         private System.Windows.Forms.TextBox tbxTotalPageCount;
         private System.Windows.Forms.TextBox tbxUrl;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbxProjectName;
+        private System.Windows.Forms.CheckBox ckbxEditProject;
     }
 }
